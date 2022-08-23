3135ed5149b6751cf442a35ca13304a4c5002613
larguesa <rlarguesa@gmail.com>
2022-08-23 09:37:46 -0300

    Added oriented object example

--- index.js
+++ index.js
@@ -13,3 +13,28 @@ document.getElementById('func-btn').onclick = function handler() {
   const res = addNumbers(getNumber('input-n1'), getNumber('input-n2'));
   displaySum(res);
 };
+//Object oriented programming
+class inputField {
+  constructor(id) {
+    this.id = id
+  }
+  getNumber() {
+    const value = document.getElementById(this.id).value
+    return parseInt(value)
+  }
+}
+class Form {
+  static outputField = document.getElementById('oo-sum-result')
+  static addNumbers(firstNumber, secondNumber) {
+    return firstNumber + secondNumber
+  }
+  static displaySum(value) {
+    this.outputField.textContent = value
+  }
+}
+const firstInput = new inputField('input-n1')
+const secondInput = new inputField('input-n2')
+document.getElementById('oo-btn').onclick = function handler() {
+  const res = Form.addNumbers(firstInput.getNumber(), secondInput.getNumber())
+  Form.displaySum(res)
+}
